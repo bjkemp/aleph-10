150e02256e41fef9bf1b8a9faafcf5d321e5eddb
Benjamin Kemp <bjkemp@gmail.com>
2025-04-01 13:23:42 -0400

    Update Jest configuration for ESM support

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,14 @@
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }]
   },
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/index.ts',
-    '!**/node_modules/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
-  },
+  coverageDirectory: "coverage",
+  setupFilesAfterEnv: ["./jest.setup.js"]
 };
